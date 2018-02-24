2549e2431b873025703cd1de3b86a729e90ed35e
grapeperson <grapeperson@inbox.ru>
2018-02-24 20:44:31 +0300

    feature/add method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strLength = str.length;
+  console.log(bracketsConfig);
+  for (let i=0;i<strLength;i++){
+    for(let x=0;x<bracketsConfig.length;x++)
+    {
+      if(str[i]==bracketsConfig[x][0] && str[i+1]==bracketsConfig[x][1]){
+        str=str.replace(bracketsConfig[x][0]+bracketsConfig[x][1],"");
+        i=-1;
+        strLength = str.length;
+      }
+    }
+  }
+  console.log(str+"строка");
+  if(str=="")return true;
+  else  return false;
 }
